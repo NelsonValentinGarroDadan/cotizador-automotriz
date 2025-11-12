f4660b30531876c057d7d20fe01b6bd5114375d0
Velentin Garro <nelsonvgarrodadan@gmail.com>
2025-11-12 12:40:59 -0300

    Update page.tsx

--- cotizador-automotriz/src/app/(CRUDS OUT WINDOWS)/administradores/[id]/view/page.tsx
+++ cotizador-automotriz/src/app/(CRUDS OUT WINDOWS)/administradores/[id]/view/page.tsx
@@ -4,23 +4,16 @@ import { useParams } from 'next/navigation';
 import { CrudPageFactory } from '@/app/components/crudPageFactory'; 
 import { User } from '@/app/types/user';
 import { useGetUserByIdQuery } from '@/app/api/userApi'; 
-import { useGetAllCompaniesQuery } from '@/app/api/companyApi';
 import AdminForm from '../../components/adminForm';
-import { Company } from '@/app/types/compay';
 
 export default function EditCompanyPage() {
   const { id } = useParams();
   const { data: user, isLoading, error } = useGetUserByIdQuery({ id: id as string });
-  const { data  } = useGetAllCompaniesQuery({ limit:50});
-  
-const FormWrapper = (props: { entity?: User; readOnly?: boolean; companies?: Company[] }) => {
-    return <AdminForm {...props} companies={data?.data ?? []} />;
-  };
 
   return (
     <CrudPageFactory<User>
       action="view"
-      formComponent={FormWrapper}
+      formComponent={AdminForm}
       entity={user}
       isLoading={isLoading}
       error={error}
