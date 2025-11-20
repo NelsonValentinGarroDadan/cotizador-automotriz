9c63bf5a5350c0cfa094917b08de541c15140a44
Velentin Garro <nelsonvgarrodadan@gmail.com>
2025-11-20 19:46:34 -0300

    Update routes.ts

--- cotizador-api/src/modules/companies/routes.ts
+++ cotizador-api/src/modules/companies/routes.ts
@@ -4,6 +4,8 @@ import { validateRequest } from "../../core/middleware/validateRequest";
 import { uploadLogo } from "../../core/middleware/uploadLogo"; 
 import * as controller from "./controller";
 import { createCompanyValidation, updateCompanySchema } from "./schema";
+import { authorizeRole } from "../../core/middleware/authorizeRole"; 
+import { Role } from "../../core/types/role";
 
 const routerCompanies = Router();
 
@@ -11,18 +13,21 @@ routerCompanies.get("/", catchAsync(controller.getAllCompanies));
 routerCompanies.get("/:id", catchAsync(controller.getCompanyById));
 routerCompanies.post(
   "/",
+  authorizeRole([Role.SUPER_ADMIN]),
   uploadLogo.single("logo"),
   validateRequest(createCompanyValidation ),
   catchAsync(controller.createCompany)
 );
 routerCompanies.put(
   "/:id",
+  authorizeRole([Role.SUPER_ADMIN]),
   uploadLogo.single("logo"),
   validateRequest(updateCompanySchema),
   catchAsync(controller.updateCompany)
 );
 routerCompanies.delete(
   "/:id",
+  authorizeRole([Role.SUPER_ADMIN]),
   catchAsync(controller.deleteCompany)
 );
 
