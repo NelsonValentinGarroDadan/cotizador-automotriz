60d13dbe33d80962316749962d885bce295d2b6d
Velentin Garro <nelsonvgarrodadan@gmail.com>
2025-11-20 20:20:59 -0300

    Update filterConfig.tsx

--- cotizador-automotriz/src/app/dashboard/administradores/components/filterConfig.tsx
+++ cotizador-automotriz/src/app/dashboard/administradores/components/filterConfig.tsx
@@ -1,22 +1,36 @@
+import { Role } from "@/app/types";
 import { Company } from "@/app/types/compay";
 import { FilterConfig } from "@/app/types/table";
 
-export const adminsFilters = ({companies}: {companies: Company[]}): FilterConfig[] => [
-  {
-    name: "search",
-    label: "Buscar por nombre o email",
-    type: "text",
-    placeholder: "Ej: Juan Pérez o admin@example.com",
-  },
-  {
-    name: "companyIds", // ✅ Este será el parámetro que se envíe al backend
-    label: "Filtrar por compañías",
-    type: "multiselect",
-    placeholder: "Selecciona una o varias compañías",
-    options: companies.map(company => ({
-      value: company.id,
-      label: company.name
-    })),
-    multiple: true,
-  }, 
-];
+export const adminsFilters = ({
+  companies,
+  role = Role.ADMIN,
+}: {
+  companies: Company[];
+  role?: Role;
+}): FilterConfig[] => {
+  const array: FilterConfig[] = [
+    {
+      name: "search",
+      label: "Buscar por nombre o email",
+      type: "text",
+      placeholder: "Ej: Juan Perez o admin@example.com",
+    },
+  ];
+
+  if (role === Role.ADMIN) {
+    array.push({
+      name: "companyIds",
+      label: "Filtrar por companias",
+      type: "multiselect",
+      placeholder: "Selecciona una o varias companias",
+      options: companies.map((company) => ({
+        value: company.id,
+        label: company.name,
+      })),
+      multiple: true,
+    });
+  }
+
+  return array;
+};
